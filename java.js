e5635017a70f086b2a6c859c1a2fdad4684c3751
Edgar <some1just@gmail.com>
2020-06-18 15:17:00 -0600

    extra stuff added

--- java.js
+++ java.js
@@ -11,7 +11,7 @@ const sampleArray = [
 
 const sections = []
 
-for (let i= 0; i < 18; i++){
+for (let i= 0; i < 23; i++){
     let heading = document.createElement('h3');
     let section = document.createElement('section')
     heading.append(` KATA ${i+1}`);
@@ -144,3 +144,55 @@ sections[16].append(kata17answer);
 let kata18answer= document.createElement('div');
 kata18answer.append(Math.max(...sampleArray));
 sections[17].append(kata18answer);
+
+//kata 19
+for(let i = 0; i < 20; i++){
+    let rect = document.createElement('div');
+    rect.style.height = '20px';
+    rect.style.width= '100px';
+    rect.style.backgroundColor= 'gray';
+    sections[18].append(rect);
+
+}
+//kata 20
+for(let i = 1; i <= 20; i++){
+    let rect = document.createElement('div');
+    rect.style.height = '20px';
+    rect.style.width = `${100+5*i}px`;
+    rect.style.backgroundColor= 'gray'
+    sections[19].append(rect);
+}
+//kata 21
+for( let value of sampleArray){
+    let rect = document.createElement('div');
+    rect.style.height = '20px';
+    rect.style.width = `${value}px`;
+    rect.style.backgroundColor = 'gray';
+    sections[20].append(rect);
+}
+
+//kata 22
+for ( let i = 0; i < sampleArray.length; i++){
+    let rect = document.createElement('div');
+    rect.style.height = '20px';
+    rect.style.width = `${sampleArray[i]}px`;
+    if (i % 2 !== 0){
+        rect.style.backgroundColor= 'red';
+    } else {
+        rect.style.backgroundColor= 'gray';
+    }
+    sections[21].append(rect);
+}
+
+//kata 23
+for (let i = 0; i <sampleArray.length; i++){
+    let rect = document.createElement('div');
+    rect.style.height= '20px';
+    rect.style.width = `${sampleArray[i]}px`;
+    if(sampleArray[i] % 2 === 0){
+        rect.style.backgroundColor = 'red';
+    }else{
+        rect.style.backgroundColor = 'gray';
+    }
+    sections[22].append(rect);
+}
